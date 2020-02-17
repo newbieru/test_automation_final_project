860384629080a9fb6ada6914262e5ce9156548e4
newbieru <duck.nt@gmail.com>
2020-02-17 21:06:53 +0500

    added: locators.py - contained public vars for locators login_page.py - contain methods for login page.
    Refactored:
    main_page.py - 'By' locators switched to 'public vars LOCATORS'

---
+++ pages/login_page.py
@@ -0,0 +1,19 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        assert 'login' in self.browser.current_url, 'The page is not login page!'
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), 'No login form founded!'
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), 'No register form founded!'
+
